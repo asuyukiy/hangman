2a4335ffda45ec9e0e8dc7190aba0190f21795a4
Shimizu Yasuyuki <yasuyuki@ShimizunoMacBook-ea.local>
2019-04-02 15:23:53 +0900

    my first comit

---
+++ hangman2.py
@@ -0,0 +1,45 @@
+list1 = ["cat" , "dog" , "banana"]
+#list1は可変
+
+import random as r
+word = list1 [r.randint(0,len(list1) - 1)]
+
+
+def hangman ():
+    wrong = 0
+    stages =[ "",
+             "________    ",
+             "|     |     ",
+             "|     |     ",
+             "|     0     ",
+             "|    /|\    ",
+             "|    / \    ",
+             "|           ",
+             ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ!")
+
+    while wrong < len(stages) - 1 :
+        print("\n")
+        msg = "1文字を予想してね"
+        char = input(msg)
+        if char in rletters :
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+            print("".join(board))
+        else:
+            wrong += 1
+            print("".join(board))
+            e = wrong + 1
+            print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("\n")
+            print("あなたの勝ち!")
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong + 1]))
+        print("あなたの負け!正解は{}。".format(word))
